4458628cbd8f56586a955c3c7dc8b2ac1a89f93c
Victor M. Varela <vmvarela@hiberus.com>
2023-08-08 16:08:01 +0000

    ..

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 module "github" {
-  source = "github.com/vmvarela/terraform-github?ref=95a2463fe225fe6331c48835c040ba911eea5d4e"
+  source = "github.com/vmvarela/terraform-github?ref=78a19b66b707395b6ffadd06bc77ef6392322671"
 
   github_owner       = var.github_owner
   github_mirror_auth = var.github_mirror_auth
